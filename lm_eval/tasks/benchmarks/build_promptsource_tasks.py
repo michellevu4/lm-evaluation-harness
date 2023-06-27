699957a8378414abc13aa343efd4ab1ae13c0e7b
lintangsutawika <lintang@sutawika.com>
2023-06-27 16:43:40 +0000

    update on how yaml file is made

--- lm_eval/tasks/benchmarks/build_promptsource_tasks.py
+++ lm_eval/tasks/benchmarks/build_promptsource_tasks.py
@@ -1,24 +1,40 @@
+import os
+import argparse
+
+from lm_eval import utils
 from promptsource.templates import DatasetTemplates
 
-T0_EVAL = [
-    ("super_glue", "wsc.fixed"),  # Coreference Resolution
-    ("winogrande", "winogrande_xl"),  # Coreference Resolution
-    ("super_glue", "cb"),  # Natural Language Inference
-    ("super_glue", "rte"),  # Natural Language Inference
-    ("anli", None),  # Natural Language Inference
-    ("super_glue", "copa"),  # Sentence Completion
-    ("hellaswag", None),  # Natural Language Inference
-    ("super_glue", "wic"),  # Word Sense Disambiguation
-]
 
-for dataset_name, subset_name in T0_EVAL:
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--benchmark", required=True)
+    parser.add_argument("--model_args", default="")
 
-    if subset_name is None:
-        prompts = DatasetTemplates(dataset_name=dataset_name)
-    else:
-        prompts = DatasetTemplates(dataset_name=dataset_name, subset_name=subset_name)
 
-    for prompt_name in prompts.all_template_names:
-        config_dict = {
-            "include: promptsource_template.yaml" "use_prompts": prompts[prompt_name]
-        }
+def main():
+    args = parse_args()
+
+    path = args.benchmark
+    yaml_path = ""
+    with open(path) as file:
+        TASK_LIST = file.readlines()
+        for dataset_name, subset_name in TASK_LIST:
+
+            if subset_name is None:
+                prompts = DatasetTemplates(dataset_name=dataset_name)
+            else:
+                prompts = DatasetTemplates(
+                    dataset_name=dataset_name, subset_name=subset_name
+                )
+
+            with open(os.path.join(yaml_path, "promptsource_template.yaml")) as file:
+                yaml_dict = file.readline()
+
+            for prompt_name in prompts.all_template_names:
+                config_dict = {
+                    "include": "promptsource_template.yaml",
+                    "use_prompts": prompts[prompt_name],
+                    **yaml_dict,
+                }
+
+                return config_dict
